5c08704e36ce9e9e44d96ecd73278a6677360867
Feroz Zargar <Ferozzargar@hotmail.com>
2022-11-23 21:06:23 -0500

    Creating checkout call from react

--- store/src/components/Navbar.js
+++ store/src/components/Navbar.js
@@ -2,6 +2,7 @@ import { Button, Container, Navbar, Modal, Nav } from "react-bootstrap";
 import { useState, useContext } from "react";
 import { CartContext } from "../CartContext";
 import CartProduct from "./CartProduct";
+import { response } from "express";
 
 function NavbarComponent() {
   const cart = useContext(CartContext);
@@ -9,6 +10,24 @@ function NavbarComponent() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const checkout = async () => {
+    await fetch("http://localhost:4000/checkout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ items: cart.items }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        if (response.url) {
+          window.location.assign(response.url); // Forwarding user to Stripe
+        }
+      });
+  };
 
   const productsCount = cart.items.reduce(
     (sum, product) => sum + product.quantity,
@@ -41,7 +60,9 @@ function NavbarComponent() {
                   ></CartProduct>
                 ))}
                 <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
-                <Button variant="success">Purchase items!</Button>
+                <Button variant="success" onClick={checkout}>
+                  Purchase items!
+                </Button>
               </p>
             </>
           ) : (
